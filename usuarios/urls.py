8df0ab1a2eeb1b4875d3f7a74f1354e88f57d56b
jon4t4s <jonatasgabrielsouzadias@gmail.com>
2024-09-12 10:39:42 -0300

    110924

---
+++ usuarios/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login, name = 'login'),
+    path('cadastro/', views.cadastro, name = 'cadastro'),
+    path('home/', views.home, name = 'home'),
+    path('lancar/', views.lancar, name = 'lancar'),
+    path('alterar/', views.alterar, name = 'alterar'),
+    path('visualizar/', views.visualizar, name = 'visualizar'),
+    path('logout/', views.logout, name = 'logout'),
+    path('excluir_verificacao/<int:pk>', views.excluir_verificacao, name = 'excluir_verificacao'),
+    path('excluir/<int:pk>', views.excluir, name = 'excluir'),
+]
